33f5e012eb2be69685d7828f6c22c9ca96a014d3
Sandip Maity <sandipmaity21052003@gmail.com>
2025-04-10 22:28:31 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,34 +1,37 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/auth.routes")
-const userRoutes = require("./routes/user.routes")
-const projectRoutes = require("./routes/project.routes")
-const blogRoutes = require("./routes/blog.routes")
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+const projectRoutes = require("./routes/project.routes");
+const blogRoutes = require("./routes/blog.routes");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: process.env.CLIENT_URI,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URI,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/blogs", blogRoutes);
-
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
